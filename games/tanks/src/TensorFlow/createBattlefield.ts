2ec06c424a023625b6e9dbe7823c44885b9006ec
Alexandr <hellbeast92@gmail.com>
2025-02-26 19:31:40 +0100

    update battlefield

--- games/tanks/src/TensorFlow/createBattlefield.ts
+++ games/tanks/src/TensorFlow/createBattlefield.ts
@@ -1,19 +1,21 @@
 import { createGame } from '../../createGame.ts';
 import { createTankRR } from '../ECS/Components/Tank.ts';
 import { random } from '../../../../lib/random.ts';
-import { floor } from '../../../../lib/math.ts';
+import { floor, sqrt } from '../../../../lib/math.ts';
 import { DI } from '../DI';
 import { getDrawState } from './utils.ts';
 
 export function createBattlefield(tanksCount: number) {
+    const rows = Math.ceil(sqrt(tanksCount));
+    const cols = Math.ceil(tanksCount / rows);
     const game = createGame();
     const width = DI.canvas.offsetWidth;
     const height = DI.canvas.offsetHeight;
 
     for (let i = 0; i < tanksCount; i++) {
         createTankRR({
-            x: (i % 3) * width / 3 + width / 3 * random(),
-            y: floor(i / 2) * height / 2 + height / 2 * random(),
+            x: (i % rows) * width / rows + width / rows * random(),
+            y: floor(i / cols) * height / cols + height / cols * random(),
             rotation: Math.PI / random(),
             color: [random(), random(), random(), 1],
         });
