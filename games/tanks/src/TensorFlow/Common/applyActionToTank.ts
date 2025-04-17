d90069e0af5ec81b9789b4f09653b6d369026510
Alexandr <hellbeast92@gmail.com>
2025-04-17 13:53:50 +0200

    next step education

--- games/tanks/src/TensorFlow/Common/applyActionToTank.ts
+++ games/tanks/src/TensorFlow/Common/applyActionToTank.ts
@@ -6,11 +6,11 @@ export function applyActionToTank(tankEid: number, action: Actions) {
     const { shoot, move, rotate, aimX, aimY } = readActions(action);
 
     TankController.setShooting$(tankEid, shoot);
-    TankController.setMove$(tankEid, clamp(TankController.move[tankEid] + move * 0.3, -0.7, 0.7));
-    TankController.setRotate$(tankEid, clamp(TankController.rotation[tankEid] + rotate * 0.3, -0.7, 0.7));
+    TankController.setMove$(tankEid, clamp(TankController.move[tankEid] + move * 0.3, -0.8, 0.8));
+    TankController.setRotate$(tankEid, clamp(TankController.rotation[tankEid] + rotate * 0.3, -0.8, 0.8));
     TankController.setTurretDir$(
         tankEid,
-        clamp(TankController.turretDir.get(tankEid, 0) + aimX * 0.3, -0.9, 0.9),
-        clamp(TankController.turretDir.get(tankEid, 1) + aimY * 0.3, -0.9, 0.9),
+        clamp(TankController.turretDir.get(tankEid, 0) + aimX * 0.3, -1.2, 1.2),
+        clamp(TankController.turretDir.get(tankEid, 1) + aimY * 0.3, -1.2, 1.2),
     );
 }
