52bf4baedbd7d527590c3e8c2c7e0f5d1764136f
Alexandr <hellbeast92@gmail.com>
2025-04-29 14:07:34 +0200

    ref: don't use upserting for models

--- games/tanks/src/TensorFlow/PPO/Player/PlayerAgent.ts
+++ games/tanks/src/TensorFlow/PPO/Player/PlayerAgent.ts
@@ -1,15 +1,12 @@
 import * as tf from '@tensorflow/tfjs';
 import '@tensorflow/tfjs-backend-wasm';
-import { createPolicyNetwork } from '../../Models/Create.ts';
 import { predict } from '../train.ts';
 import { InputArrays } from '../../Common/InputArrays.ts';
-import { macroTasks } from '../../../../../../lib/TasksScheduler/macroTasks.ts';
-import { setModelState } from '../../Common/modelsCopy.ts';
-import { loadNetworkFromDB, Model } from '../../Models/Transfer.ts';
-import { disposeNetwork } from '../../Models/Utils.ts';
+import { Model } from '../../Models/Transfer.ts';
+import { disposeNetwork, getNetwork } from '../../Models/Utils.ts';
 
 export class PlayerAgent {
-    private policyNetwork: tf.LayersModel = createPolicyNetwork();
+    private policyNetwork?: tf.LayersModel;
 
     constructor() {
     }
@@ -19,37 +16,22 @@ export class PlayerAgent {
     }
 
     getVersion() {
-        return this.policyNetwork.optimizer.iterations;
+        return this.policyNetwork?.optimizer.iterations ?? 0;
     }
 
     predict(state: InputArrays): { actions: Float32Array } {
         return predict(
-            this.policyNetwork,
+            this.policyNetwork!,
             state,
         );
     }
 
     async sync() {
-        while (!(await this.load())) {
-            await new Promise((resolve) => macroTasks.addTimeout(resolve, 1000));
-        }
+        return this.load();
     }
 
     private async load() {
-        try {
-            const policyNetwork = await loadNetworkFromDB(Model.Policy);
-
-            if (!policyNetwork) return false;
-
-            this.policyNetwork = await setModelState(this.policyNetwork, policyNetwork);
-
-            disposeNetwork(policyNetwork);
-
-            console.log('[PlayerAgent] Models loaded successfully');
-            return true;
-        } catch (error) {
-            console.warn('[PlayerAgent] Could not load models, starting with new ones:', error);
-            return false;
-        }
+        this.policyNetwork && disposeNetwork(this.policyNetwork);
+        this.policyNetwork = await getNetwork(Model.Policy);
     }
 }
