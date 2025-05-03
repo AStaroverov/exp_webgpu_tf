eaeb0e89b783cacd27922b3a150857df478dee04
Alexandr <hellbeast92@gmail.com>
2025-05-03 15:30:09 +0200

    ref: loss checker

--- games/tanks/src/TensorFlow/PPO/Learner/createLossChecker.ts
+++ games/tanks/src/TensorFlow/PPO/Learner/createLossChecker.ts
@@ -1,20 +1,23 @@
 import { RingBuffer } from 'ring-buffer-ts';
-import { abs, mean } from '../../../../../../lib/math.ts';
+import { max, min } from '../../../../../../lib/math.ts';
 import { clamp } from 'lodash-es';
 
 export function createLossChecker() {
-    let lastMean = 0;
     const history = new RingBuffer<number>(100);
 
     return {
         check: (losses: number[]) => {
             if (history.getBufferLength() < 10) return true;
 
-            const shift = abs(lastMean) * 500;
-            const result = losses.every(loss => loss === clamp(loss, lastMean - shift, lastMean + shift));
+            const arr = history.toArray();
+            const minLoss = min(...arr);
+            const maxLoss = max(...arr);
+            const diff = maxLoss - minLoss;
+            const shift = diff * 500;
+
+            const result = losses.every(loss => loss === clamp(loss, minLoss - shift, maxLoss + shift));
 
             history.add(...losses);
-            lastMean = mean(history.toArray());
 
             return result;
         },
