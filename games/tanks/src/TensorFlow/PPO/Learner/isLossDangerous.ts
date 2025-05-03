5ea2cd22bdd407dcdc16fcaa37146e1014cad5d7
Alexandr <hellbeast92@gmail.com>
2025-05-03 18:33:31 +0200

    fix: decrease loss limti

--- games/tanks/src/TensorFlow/PPO/Learner/isLossDangerous.ts
+++ games/tanks/src/TensorFlow/PPO/Learner/isLossDangerous.ts
@@ -1,5 +1,5 @@
 import { abs } from '../../../../../../lib/math.ts';
 
 export function isLossDangerous(loss: number): boolean {
-    return !Number.isFinite(loss) || abs(loss) > 1e4;
+    return !Number.isFinite(loss) || abs(loss) > 50;
 }
