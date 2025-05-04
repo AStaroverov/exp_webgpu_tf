81acf3d30f69693504768a9f86cdd2e7b1b22e36
Alexandr <hellbeast92@gmail.com>
2025-05-04 14:59:27 +0200

    ref: refactoring

--- games/tanks/src/TensorFlow/Common/applyActionToTank.ts
+++ games/tanks/src/TensorFlow/Common/applyActionToTank.ts
@@ -13,18 +13,42 @@ export function applyActionToTank(
     limitRotation = 1,
     limitAimDir = 2,
 ) {
-    const shoot = (1 - probability[0]) * TankController.shoot[tankEid] + (probability[0] * actions[0]);
-    const move = (1 - probability[1]) * TankController.move[tankEid] + (limitMove * probability[1] * actions[1]);
-    const rotate = (1 - probability[2]) * TankController.rotation[tankEid] + (limitRotation * probability[2] * actions[2]);
-    const aimX = (1 - probability[3]) * TankController.turretDir.get(tankEid, 0) + (limitAimDir * probability[3] * actions[3]);
-    const aimY = (1 - probability[4]) * TankController.turretDir.get(tankEid, 1) + (limitAimDir * probability[4] * actions[4]);
-
-    TankController.setShooting$(tankEid, clamp(shoot, -1, 1));
-    TankController.setMove$(tankEid, clamp(move, -limitMove, limitMove));
-    TankController.setRotate$(tankEid, clamp(rotate, -limitRotation, limitRotation));
-    TankController.setTurretDir$(
-        tankEid,
-        clamp(aimX, -limitAimDir, limitAimDir),
-        clamp(aimY, -limitAimDir, limitAimDir),
+    const shoot = blendLinear(
+        TankController.shoot[tankEid],
+        actions[0],
+        probability[0],
     );
+    const move = blendLinear(
+        TankController.move[tankEid],
+        actions[1],
+        probability[1],
+        limitMove,
+    );
+    const rotate = blendLinear(
+        TankController.rotation[tankEid],
+        actions[2],
+        probability[2],
+        limitRotation,
+    );
+    const aimX = blendLinear(
+        TankController.turretDir.get(tankEid, 0),
+        actions[3],
+        probability[3],
+        limitAimDir,
+    );
+    const aimY = blendLinear(
+        TankController.turretDir.get(tankEid, 1),
+        actions[4],
+        probability[4],
+        limitAimDir,
+    );
+    TankController.setShooting$(tankEid, shoot);
+    TankController.setMove$(tankEid, move);
+    TankController.setRotate$(tankEid, rotate);
+    TankController.setTurretDir$(tankEid, aimX, aimY);
+}
+
+function blendLinear(prev: number, raw: number, p: number, limit = 1) {
+    const next = prev * (1 - p) + raw * limit * p;
+    return clamp(next, -limit, limit);
 }
