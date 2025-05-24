3d443ecf83d00485e2405051d13d7099b437d065
Alexandr <hellbeast92@gmail.com>
2025-05-24 18:50:43 +0200

    refactor: update physical functions

--- games/tanks/src/Game/Physical/setPhysicalCollisionGroup.ts
+++ games/tanks/src/Game/Physical/setPhysicalCollisionGroup.ts
@@ -2,7 +2,7 @@ import { GameDI } from '../DI/GameDI.ts';
 import { RigidBodyRef } from '../ECS/Components/Physical.ts';
 import { CollisionGroup } from './createRigid.ts';
 
-export function resetCollisionsTo(eid: number, group: CollisionGroup, { physicalWorld } = GameDI) {
+export function setPhysicalCollisionGroup(eid: number, group: CollisionGroup, { physicalWorld } = GameDI) {
     const physicalId = RigidBodyRef.id[eid];
     const rigidBody = physicalWorld.getRigidBody(physicalId);
 
