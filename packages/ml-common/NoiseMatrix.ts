2b8f04fee13a11a18f1e4379dbda247eca36ccd5
AStaroverov <hellbeast92@gmail.com>
2025-10-21 16:56:00 +0200

    refactor: simplify resample method and remove variant handling in NoiseMatrix

--- packages/ml-common/NoiseMatrix.ts
+++ packages/ml-common/NoiseMatrix.ts
@@ -12,22 +12,12 @@ export class NoiseMatrix {
     ) {
     }
 
-    /**
-     * Пересэмплирование матрицы шума Θ
-     */
     resample() {
         this.Theta?.dispose();
-
-        if (this.variantA) {
-            // Вариант A: Θ ~ N(0, I)
-            this.Theta = tf.randomNormal([this.latentDim, this.actionDim]) as tf.Tensor2D;
-        } else {
-            // Вариант B: Θ ~ N(0, diag(σ_base^2))
-            this.Theta = tf.tidy(() => {
-                const baseStd = tf.exp(this.logStdBase).reshape([1, this.actionDim]);
-                return tf.randomNormal([this.latentDim, this.actionDim]).mul(baseStd) as tf.Tensor2D;
-            });
-        }
+        this.Theta = tf.tidy(() => {
+            const baseStd = tf.exp(this.logStdBase).reshape([1, this.actionDim]);
+            return tf.randomNormal([this.latentDim, this.actionDim]).mul(baseStd) as tf.Tensor2D;
+        });
     }
 
     maybeResample() {
@@ -41,15 +31,7 @@ export class NoiseMatrix {
             throw new Error('NoiseMatrix: Theta is not initialized. Call resample() first.');
         }
 
-        const raw = phi.matMul(this.Theta!) as tf.Tensor2D; // [B, A]
-
-        if (this.variantA) {
-            // Применяем базовое std после умножения
-            const baseStd = tf.exp(this.logStdBase).reshape([1, this.actionDim]);
-            return raw.mul(baseStd) as tf.Tensor2D;
-        }
-
-        return raw;
+        return phi.matMul(this.Theta!) as tf.Tensor2D; // [B, A]
     }
 
     dispose() {
