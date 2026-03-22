9256065f3136097412278a598c8f8cfed624631e
AStaroverov <hellbeast92@gmail.com>
2026-03-22 11:33:33 +0300

    refactor: adjust building placement logic in diagonal wall scenario for improved layout

--- packages/ml-common/Curriculum/createBattlefield.ts
+++ packages/ml-common/Curriculum/createBattlefield.ts
@@ -1,19 +1,22 @@
+import { clamp, isNumber } from 'lodash-es';
+import { lerp } from '../../../lib/math.ts';
 import { createGame } from '../../tanks/src/Game/createGame.ts';
 import { createMLPlugin } from '../../tanks/src/Plugins/ML/createMlPlugin.ts';
 import { createPilotsPlugin } from '../../tanks/src/Plugins/Pilots/createPilotsPlugin.ts';
+import { LEARNING_STEPS } from '../consts.ts';
 
 const MAX_SIZE = 1600;
-const MIN_SIZE = 1000;
+const MIN_SIZE = 600;
 
 export function createBattlefield(options?: { size?: number, iteration?: number }) {
-    const size = options?.size ?? 1000;
-    // lerp(
-    //     MIN_SIZE,
-    //     MAX_SIZE,
-    //     isNumber(options?.iteration)
-    //         ? clamp((options.iteration / LEARNING_STEPS), 0, 1)
-    //         : 1
-    // );
+    const size = //options?.size ?? 1000;
+    lerp(
+        MIN_SIZE,
+        MAX_SIZE,
+        isNumber(options?.iteration)
+            ? clamp((options.iteration / LEARNING_STEPS), 0, 1)
+            : 1
+    );
     const game = createGame({ width: size, height: size });
     const pilots = createPilotsPlugin(game);
     const ml = createMLPlugin(game);
