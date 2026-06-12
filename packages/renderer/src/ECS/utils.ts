77d843779aa24051d40b4f1a75d9f325cf528dc1
AStaroverov <hellbeast92@gmail.com>
2026-06-12 13:19:04 +0200

    feat: refactor component definition to support subcomponents and enhance context management

--- packages/renderer/src/ECS/utils.ts
+++ packages/renderer/src/ECS/utils.ts
@@ -5,16 +5,44 @@ const $CompRef = Symbol("CompRef");
 let indexCompRef = 0;
 let nextCompRef: any = { [$CompRef]: indexCompRef++ };
 
-export function component<T>(_comp: T): T {
-  const comp = Object.assign(nextCompRef, _comp);
-  nextCompRef = { [$CompRef]: indexCompRef++ };
-  return comp;
-}
-
 type ReactiveSetter = (eid: number, ...args: any[]) => any;
+
 export type Obs = <F extends ReactiveSetter>(setter: F) => F;
 
-export function defineComponent<T extends object>(create: (ref: object, obs: Obs) => T) {
+export type ComponentContext = { obs: Obs; world: World };
+
+export type SubComponent<T extends object, A extends unknown[] = []> = (
+  component: object,
+  ctx: ComponentContext,
+  ...args: A
+) => T;
+
+const subComponentRegistry = new WeakMap<World, WeakMap<SubComponent<any, any>, object[]>>();
+
+export function getComponents<T extends object>(
+  world: World,
+  constr: SubComponent<T, any>,
+): (T & object)[] {
+  return (subComponentRegistry.get(world)?.get(constr) ?? []) as (T & object)[];
+}
+
+export function defineSubComponent<T extends object, A extends unknown[] = []>(
+  create: SubComponent<T, A>,
+): SubComponent<T, A> {
+  const constr: SubComponent<T, A> = (component, ctx, ...args) => {
+    const byConstr =
+      subComponentRegistry.get(ctx.world) ?? new WeakMap<SubComponent<any, any>, object[]>();
+    byConstr.set(constr, (byConstr.get(constr) ?? []).concat(component));
+    subComponentRegistry.set(ctx.world, byConstr);
+    return create(component, ctx, ...args);
+  };
+
+  return constr;
+}
+
+export function defineComponent<T extends object>(
+  create: (ref: object, ctx: ComponentContext) => T,
+) {
   return (world: World): T => {
     const ref = nextCompRef as object;
     const localObs: Obs = <F extends ReactiveSetter>(setter: F): F => {
@@ -25,7 +53,7 @@ export function defineComponent<T extends object>(create: (ref: object, obs: Obs
         return result;
       }) as F;
     };
-    const comp = Object.assign(ref, create(ref, localObs)) as T;
+    const comp = Object.assign(ref, create(ref, { obs: localObs, world })) as T;
     nextCompRef = { [$CompRef]: indexCompRef++ };
     return comp;
   };
